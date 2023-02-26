8d20831ba58ad90fecfc6a89ddbac77e6586f255
Tade77 <adekunletaofeeq@mail.com>
2023-02-26 13:09:57 +0100

    HomePage set-up

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1 style={{ fontSize: "36px", color: "blue" }}>Quick Quiz</h1>
+
+      <button className="btns">Play</button>
+      <button className="btns">High score</button>
+    </div>
+  );
+};
+export default Home;
